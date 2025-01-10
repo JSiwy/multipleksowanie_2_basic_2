88ad15c59a3dacde1ab664264506341f9e7a87d7
JSiwy <siwy.jann@gmail.com>
2025-01-10 19:48:37 +0100

    komentarze

--- Nastawnik.cpp
+++ Nastawnik.cpp
@@ -1,11 +1,12 @@
 #include "Nastawnik.h"
 
+//Obiekt klasy Nastawnik, towrzy listę 48 pozycyjną
 Nastawnik::Nastawnik() {
   for (int i = 0; i < 49; i++) {
     positions[i].push_back({ -1 });
   }
 }
-
+//Dodaje pozycję do listy pozycji razem z indeksem
 void Nastawnik::addPosition(int index, long values[]) {
   if (index >= 0 && index < positions.size()) {
     Vector<long> val;
@@ -15,7 +16,7 @@ void Nastawnik::addPosition(int index, long values[]) {
     positions[index] = val;
   }
 }
-
+//Zwraca pozycję indeks pozycji na podstawie wartości
 int Nastawnik::getPos(long value) {
   for (int i = 0; i < positions.size(); i++) {
     for (int j = 0; j < positions[i].size(); j++) {
